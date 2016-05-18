84e3487b49c57d76a8e75b60677c8a19e850d459
HKobe <HGKobeissi1@gmail.com>
2016-05-18 13:39:18 -0400

    COMP125-S2016-Assignment1

--- COMP123-Lesson1-VSCode/Scriptss/apps.js
+++ COMP123-Lesson1-VSCode/Scriptss/apps.js
@@ -1,3 +1,32 @@
-//custom JavaScript
+//Main JavaScript File
+//This is a global scope
+//IIFE Imemediately Inovked Function Expression
+// Structure, Style/presentation, Behaviour
+(function(){ 
+    "Use Strict";
+    
+    //codes in here
+/*Console.log("App Started...");
+var firstParagraph = document.getElementById("firstParagraph");
+firstParagraph.textContent =  "";
 
-Console.log("App Started...");
+   //declare named function
+function writeToFirstParagraph() {
+    //for debugging only
+    console.log("Executing writeToFirstParagraph")
+    //create a reference to a p element with an id of "firstParagraph
+    var firstParagraph = document.getElementById("firstParagraph");
+    firstParagraph.textContent = "My name is Hassan Kobeissi, I'm a Software Engineer at Centennial College.";
+}*/
+//declare a variable that's pointing towards an anonymous function.
+var writeToFirstParagraph = function() {
+     //for debugging only
+    console.log("Executing writeToFirstParagraph")
+    //create a reference to a p element with an id of "firstParagraph
+    var firstParagraph = document.getElementById("firstParagraph");
+    firstParagraph.textContent = "My name is Hassan Kobeissi, I'm a Software Engineer at Centennial College.";
+    
+}
+
+
+})();
